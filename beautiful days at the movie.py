9fd79dcc57a1db3b26642c7e7dc2e64c498e17ba
Razane Marref <70075825+MarRazane@users.noreply.github.com>
2023-02-19 21:26:07 +0100

    Add files via upload

---
+++ beautiful days at the movie.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    
+    B_day = 0
+    for day in range(i, j+1):
+        reverse = int(str(day)[::-1])
+        if abs(reverse-day)%k==0:
+            B_day +=1 
+         
+    return B_day
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
